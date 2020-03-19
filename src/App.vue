dffe17002919d07dfca07ac07d5f6dcc38bdf74e
Chandrakanth <chanduvemakoti@gmail.com>
2020-03-19 15:23:15 +0530

    basic

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header/>
+   <Registration/>
+  </div>
+</template>
+
+<script>
+
+import Registration from './components/Registration.vue'
+import Header from './components/Header.vue'
+export default {
+  name: 'App',
+  components: {
+    Registration,
+    Header
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
